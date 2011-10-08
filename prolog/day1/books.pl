1c9af461708c2569cfb15f6390467241a48098e4
Hector Barriuso <hbarriuso@gmail.com>
2011-10-08 18:17:40 +0200

    Books written by author knowledge base for day 1 practice

---
+++ prolog/day1/books.pl
@@ -0,0 +1,6 @@
+writes(tate, seven_languages_in_seven_weeks).
+writes(tolkien, the_hobbit).
+writes(tolkien, the_lord_of_the_rings).
+writes(lovecraft, myths_of_chtulhu).
+writes(pratchett, color_of_magic).
+writes(pratchett, guards_guards).
