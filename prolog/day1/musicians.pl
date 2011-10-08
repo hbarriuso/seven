baa1afd46640c46ea38b255d20894e5b169c3fc7
Hector Barriuso <hbarriuso@gmail.com>
2011-10-08 18:27:27 +0200

    Musicians knowledge base for day 1 practice

---
+++ prolog/day1/musicians.pl
@@ -0,0 +1,10 @@
+plays(armstrong, trumpet).
+plays(kenny, saxo).
+plays(springsteen, guitar).
+plays(paredes, guitar).
+plays(de_lucia, guitar).
+
+music_style(armstrong, jazz).
+music_style(kenny, jazz).
+music_style(springsteen, rock).
+music_style(de_lucia, flamenco).
