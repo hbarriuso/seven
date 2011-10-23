7f7e1374e628a0537da743f37fa772f56307b7a2
Hector Barriuso <hbarriuso@gmail.com>
2011-10-23 18:34:55 +0200

    Monitor for translate_service to restart it as it dies for day 3 practice

---
+++ erlang/day3/monitor_translate.erl
@@ -0,0 +1,15 @@
+-module(monitor_translate).
+-export([loop/0]).
+loop() ->
+    process_flag(trap_exit, true), 
+    receive
+        new ->
+            io:format("Creating and monitoring process.~n"),
+            register(translator, spawn_link(fun translate_service:loop/0)),
+            loop();
+        {'EXIT', From, Reason} -> 
+            io:format("The translator ~p died with reason ~p.", [From, Reason]),
+			io:format(" Restarting. ~n"),
+            self() ! new, 
+            loop()
+        end.
